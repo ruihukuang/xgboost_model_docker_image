5a3d13ee0c65db96d19b4fb526693ba5eee7fc04
ruihukuang <44253429+ruihukuang@users.noreply.github.com>
2025-07-16 11:30:47 +1000

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 bind = "0.0.0.0:8000"
 
 # Number of worker processes
-workers = 4  # Adjust based on your application's needs and server capacity
+workers = 2  # Adjust based on your application's needs and server capacity
 
 # Timeout settings
 timeout = 900  # Worker timeout in seconds
