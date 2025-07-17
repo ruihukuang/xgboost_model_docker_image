09f827b19d33de0bcf62fe16576ee400069558de
ruihukuang <44253429+ruihukuang@users.noreply.github.com>
2025-07-17 12:29:06 +1000

    Update wsgi.py

--- scripts_nonmodel/wsgi.py
+++ scripts_nonmodel/wsgi.py
@@ -1,4 +1,4 @@
-import scripts_nonmodel.predictor as myapp
+import scripts_nonmodel.predition as myapp
 
 # This is just a simple wrapper for gunicorn to find your app.
 # If you want to change the algorithm file, simply change "predictor" above to the
